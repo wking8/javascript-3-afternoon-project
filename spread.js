a01ccc0fc9f242d370fd573cbbfe745017a93e76
Wyatt King <wyattking@Wyatts-MacBook-Pro.local>
2019-07-02 08:49:12 -0600

    finish spread

--- spread.js
+++ spread.js
@@ -17,7 +17,7 @@ let product = {
   imageUrl: 'https://i.ebayimg.com/images/g/9T4AAOSwbmdZf2jN/s-l640.jpg'
 }
 
-const productCopy = [...product]
+const productCopy = {...product}
 
 
 
@@ -28,7 +28,7 @@ const productCopy = [...product]
 
 let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-// code here
+const colorsCopy = [...colors]
 
 
 
@@ -43,7 +43,7 @@ function addNums(num1, num2, num3) {
   return num1 + num2 + num3;
 }
 
-// code here
+const result = addNums(...numbers)
 
 
 ////////// PROBLEM 4 //////////
@@ -53,4 +53,4 @@ function addNums(num1, num2, num3) {
 let arr1 = ['dog', 'chameleon', 'monkey'];
 let arr2 = ['bear', 'tiger', 'liger'];
 
-// code here
+const combinedArrays = [...arr1, ...arr2]
